4f4e28b8e6386889a804e763505eb770b625c232
lrodrin <lrodrin@gmail.com>
2019-04-26 13:28:42 +0200

    update client

---
+++ AROF/agent/client/client.py
@@ -0,0 +1,52 @@
+import requests
+
+host_1 = "10.1.7.65"
+host_2 = "10.1.7.67"
+headers = {"Content-Type": "application/json"}
+
+
+def get(host):
+    """
+    Retrieve operations on the Laser
+
+    :param host: ip address from REST API agent
+    :return:
+    """
+    request = requests.get('http://%s:5001/api/arof' % host, headers=headers)
+    return request.json()
+
+
+def post(host, id):
+    payload = {'enable': True}
+    request = requests.post('http://%s:5001/api/arof/%s' % (host, id), headers=headers, params=payload)
+    return request.json()
+
+
+def put(host, id):
+    payload = {'enable': False}
+    request = requests.post('http://%s:5001/api/arof/%s' % (host, id), headers=headers, params=payload)
+    return request.json()
+
+
+def delete(host, id):
+    request = requests.delete('http://%s:5001/api/arof/%s' % (host, id), headers=headers)
+    return request
+
+
+if __name__ == '__main__':
+    # POST
+    print(post(host_1, 2))
+    print(post(host_2, 2))
+    # GET
+    print(get(host_1))
+    print(get(host_2))
+    # PUT
+    print(put(host_1, 2))
+    print(put(host_2, 2))
+    # GET
+    print(get(host_1))
+    print(get(host_2))
+
+    # DELETE
+    print(delete(host_1, 2))
+    print(delete(host_2, 2))
