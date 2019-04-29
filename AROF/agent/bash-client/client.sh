ba1f6dbdbbd90ce0f59a4a20bc00cb192a1d5044
lrodrin <lrodrin@gmail.com>
2019-04-29 13:02:41 +0200

    add bash client

--- AROF/agent/bash-client/client.sh
+++ AROF/agent/bash-client/client.sh
@@ -8,13 +8,13 @@ header1="Accept: application/json"
 header2="Content-Type: application/json"
 stp="28508ab5-9591-47ed-9445-d5e8e9bafff6"
 
-function post {
+function exec_post {
         curl --verbose -H \"${header1}\" -H \"${header2}\" -X PUT -d '{\"id\": \"$stp\"}' ${start_url}
 }
 
-function get {
+function exec_get {
         curl -X PUT $stop_url
 }
 
-post
-get
+exec_post
+exec_get
