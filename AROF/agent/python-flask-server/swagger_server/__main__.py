2a7e498eacbdb0162447039111abcf60056a44fd
lrodrin <lrodrin@gmail.com>
2019-04-30 10:30:05 +0200

    add logging control into arof rest server

--- AROF/agent/python-flask-server/swagger_server/__main__.py
+++ AROF/agent/python-flask-server/swagger_server/__main__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import logging
+from logging.handlers import RotatingFileHandler
 
 import connexion
 
@@ -9,6 +11,12 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'AROF API'})
+    handler = RotatingFileHandler('python-flask-server.log', maxBytes=1000000, backupCount=5)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.DEBUG)
+    log.addHandler(handler)
     app.run(port=5001)
 
 
