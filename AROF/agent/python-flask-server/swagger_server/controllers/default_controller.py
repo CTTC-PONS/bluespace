723bef8dd672e770a5f74ee071a60f40aaccf139
lrodrin <lrodrin@gmail.com>
2019-07-04 10:18:46 +0200

    updated controller and database. arod-conf funtion is called just in case the enable parameter will be true in POST and PUT methods

--- AROF/agent/python-flask-server/swagger_server/controllers/default_controller.py
+++ AROF/agent/python-flask-server/swagger_server/controllers/default_controller.py
@@ -15,8 +15,10 @@ def create_configuration_by_id(arof_id, enable=False):  # noqa: E501
 
     :rtype: Operation
     """
-    exec_config_app(arof_id, enable)
-    return database.create_operation(arof_id, enable)
+    new_op = database.create_operation(arof_id, enable)
+    if new_op.enable is True:
+        exec_config_app(arof_id, enable)
+    return new_op
 
 
 def delete_configuration_by_id(arof_id):  # noqa: E501
@@ -26,8 +28,6 @@ def delete_configuration_by_id(arof_id):  # noqa: E501
 
     :param arof_id: arof id
     :type arof_id: int
-
-    :rtype: None
     """
     database.delete_operation(arof_id)
 
@@ -35,7 +35,7 @@ def delete_configuration_by_id(arof_id):  # noqa: E501
 def retrieve_configuration():  # noqa: E501
     """Retrieve configuration
 
-    Retrieve operation of resource: laser # noqa: E501
+    Retrieve operations of resource: laser # noqa: E501
 
     :rtype: List[Operation]
     """
@@ -54,11 +54,24 @@ def update_configuration_by_id(arof_id, enable=False):  # noqa: E501
 
     :rtype: Operation
     """
-    exec_config_app(arof_id, enable)
-    return database.update_operation(arof_id, enable)
+    op = database.update_operation(arof_id, enable)
+    if op.enable is True:
+        exec_config_app(arof_id, enable)
+    return op
 
 
 def exec_config_app(arof_id, enable):
+    """Execute configuration application
+
+    Call application that is responsible to configure the actual ARoF HW   
+
+    :param arof_id: arof id
+    :type arof_id: int
+    :param enable: enable or disable the laser
+    :type enable: bool
+
+    :rtype:
+    """
     # call_arg_list = ["swagger_server/arof-conf/arof-conf", "-i", "{:d}".format(arof_id), "-e", "{:d}".format(enable)]
     call_arg_list = ["swagger_server/arof-conf/arof-conf", "-v", "-i", "{:d}".format(arof_id), "-e", "{:d}".format(enable)]
     # print (['CMD:', call_arg_list])
--- AROF/agent/python-flask-server/swagger_server/database.py
+++ AROF/agent/python-flask-server/swagger_server/database.py
@@ -12,30 +12,31 @@ def create_operation(arof_id, enable):
     :param enable: enable or disable the laser
     :type enable: bool
 
+    :return: new operation
     :rtype: Operation
     """
-    if len(operations) != 0:
-        index = next((index for (index, op) in enumerate(operations) if op.arof_id == arof_id), None)
-        if index is None:
-            return add_operation(arof_id, enable)
+    if len(operations) != 0: # if exists operations
+        if not any(id for (id, op) in enumerate(operations) if op.arof_id == arof_id): # if not exists arof_id in operations
+            return new_operation(arof_id, enable)
     else:
-        return add_operation(arof_id, enable)
+        return new_operation(arof_id, enable)
 
 
-def add_operation(arof_id, enable):
+def new_operation(arof_id, enable):
     """
-    Create and add operation
+    Create and add operation to operations
 
     :param arof_id: arof id
     :type arof_id: int
     :param enable: enable or disable the laser
     :type enable: bool
 
+    :return: new operation
     :rtype: Operation
     """
-    op = Operation(arof_id, enable)
-    operations.append(op)
-    return op    
+    new_op = Operation(arof_id, enable) 
+    operations.append(new_op)   
+    return new_op    
 
 
 def update_operation(arof_id, enable):
@@ -47,14 +48,15 @@ def update_operation(arof_id, enable):
     :param enable: enable or disable the laser
     :type enable: bool
 
+    :return: operation modified
     :rtype: Operation
     """
-    if len(operations) != 0:
-        for item in operations:
-            if item.arof_id == arof_id:
-                item.arof_id = arof_id
-                item.enable = enable
-                return item
+    if len(operations) != 0:    # if exists operations
+        for op in operations:
+            if op.arof_id == arof_id:
+                op.arof_id = arof_id
+                op.enable = enable
+                return op
 
 
 def delete_operation(arof_id):
@@ -64,7 +66,7 @@ def delete_operation(arof_id):
     :param arof_id: arof id
     :type arof_id: int
     """
-    if len(operations) != 0:
-        for item in operations:
-            if item.arof_id == arof_id:
-                operations.remove(item)
+    if len(operations) != 0:    # if exists operations
+        for op in operations:
+            if op.arof_id == arof_id:
+                operations.remove(op)
