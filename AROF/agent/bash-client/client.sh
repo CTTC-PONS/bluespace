1d0bd7118ad57f24af444b322d31d63fd7adcab0
Evangelos Grivas <vage@eulambia.com>
2020-02-14 12:09:38 +0200

    Added 'hostname' and 'port' variables to  bash clients

--- AROF/agent/bash-client/client.sh
+++ AROF/agent/bash-client/client.sh
@@ -2,32 +2,33 @@
 
 header1="Content-Type:application/json"
 header2="Accept:application/json"
-hostname="cyclone5"
+hostname="192.168.1.94"
+port="5001"
 
 function exec_post {
-        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/0?enable=false"
-        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/1?enable=false"
-        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/2?enable=false"
-        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/3?enable=false"
+        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/0?enable=false"
+        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/1?enable=false"
+        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/2?enable=false"
+        curl -X POST --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/3?enable=false"
 }
 
 function exec_get {
-        curl -X GET --header ${header2} "http://${hostname}:5001/api/arof"
+        curl -X GET --header ${header2} "http://${hostname}:${port}/api/arof"
 }
 
 function exec_put {
 
-        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/0?enable=true"
-        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/1?enable=true"
-        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/2?enable=true"
-        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:5001/api/arof/3?enable=true"
+        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/0?enable=true"
+        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/1?enable=true"
+        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/2?enable=true"
+        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/3?enable=true"
 }
 
 function exec_delete {
-        curl -X DELETE --header ${header2} "http://${hostname}:5001/api/arof/0"
-        curl -X DELETE --header ${header2} "http://${hostname}:5001/api/arof/1"
-        curl -X DELETE --header ${header2} "http://${hostname}:5001/api/arof/2"
-        curl -X DELETE --header ${header2} "http://${hostname}:5001/api/arof/3"
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/arof/0"
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/arof/1"
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/arof/2"
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/arof/3"
 }
 
 echo "ENSURE LASERS OFF"
--- OBFN/agent/bash-client/client.sh
+++ OBFN/agent/bash-client/client.sh
@@ -2,21 +2,23 @@
 
 header1="Content-Type:application/json"
 header2="Accept:application/json"
+hostname="192.168.1.94"
+port="5002"
 
 function exec_post {
-        curl -X POST --header ${header1} --header ${header2} -d '{"operations": [{"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 0, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 1, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 2, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 3, "wavelength": 1553}]}' 'http://localhost:5002/api/obfn'
+        curl -X POST --header ${header1} --header ${header2} -d '{"operations": [{"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 0, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 1, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 2, "wavelength": 1553}, {"x_offset_angle": -90, "y_offset_angle": 90, "beam_id": 3, "wavelength": 1553}]}' "http://${hostname}:${port}/api/obfn"
 }
 
 function exec_get {
-        curl -X GET --header ${header2} 'http://localhost:5002/api/obfn'
+        curl -X GET --header ${header2} "http://${hostname}:${port}/api/obfn"
 }
 
 function exec_put {
-        curl -X PUT --header ${header1} --header ${header2} 'http://localhost:5002/api/obfn/1?x_offset_angle=-80&y_offset_angle=80&wavelength=1552'
+        curl -X PUT --header ${header1} --header ${header2} "http://${hostname}:${port}/api/obfn/1?x_offset_angle=-80&y_offset_angle=80&wavelength=1552"
 }
 
 function exec_delete {
-        curl -X DELETE --header ${header2} 'http://localhost:5002/api/obfn'
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/obfn"
 }
 
 echo "POST"
