24b79aa76a14df8327703f4a6cacf9f68bc26584
Evangelos Grivas <48053733+vagegr@users.noreply.github.com>
2020-05-04 13:00:57 +0300

    Updated arof bash-client to match v2.0

--- AROF/agent/bash-client/client.sh
+++ AROF/agent/bash-client/client.sh
@@ -2,8 +2,8 @@
 
 header1="Content-Type:application/json"
 header2="Accept:application/json"
-hostname="localhost"                    # set to "$1" to pass this as argument
-port="5000"                             # similarly set to "$2" to pass this as 2nd arg
+hostname="$1"                           # set to "$1" to pass this as argument
+port="$2"                               # similarly set to "$2" to pass this as 2nd arg
 
 # function exec_post {
 #         curl -X POST --header ${header1} --header ${header2} "http://${hostname}:${port}/api/arof/0?enable=false"
@@ -15,27 +15,27 @@ port="5000"                             # similarly set to "$2" to pass this as 
 function exec_post {
 
         curl --request POST \
-        --url "http://${hostname}:${port}/api/arof" \
+        --url "http://${hostname}:${port}/arof" \
         --header ${header1} \
         --data '{
                 "arof-pool": [
                         {
-                                "arof-id": 0,
+                                "id": 0,
                                 "enabled": false,
                                 "wavelength": 1
                         },
                         {
-                                "arof-id": 1,
+                                "id": 1,
                                 "enabled": false,
                                 "wavelength": 1
                         },
                         {
-                                "arof-id": 2,
+                                "id": 2,
                                 "enabled": false,
                                 "wavelength": 1
                         },
                         {
-                                "arof-id": 3,
+                                "id": 3,
                                 "enabled": false,
                                 "wavelength": 1
                         }
@@ -45,7 +45,7 @@ function exec_post {
 
 
 function exec_get {
-        curl --request GET --url "http://${hostname}:${port}/api/arof"
+        curl --request GET --url "http://${hostname}:${port}/arof"
 }
 
 function exec_put {
@@ -57,7 +57,7 @@ function exec_put {
 # }
 
         curl --request PUT \
-        --url http://${hostname}:${port}/api/arof \
+        --url http://${hostname}:${port}/arof \
         --header ${header1} \
         --data '{
                 "arof-pool": [
@@ -78,6 +78,38 @@ function exec_put {
                         },
                         {
                                 "arof-id": 3,
+                                "enabled": true,
+                                "wavelength": 2
+                        }
+
+                ]
+        }'
+}
+
+function exec_post_set {
+
+        curl --request POST \
+        --url http://${hostname}:${port}/arof \
+        --header ${header1} \
+        --data '{
+                "arof-pool": [
+                        {
+                                "id": 0,
+                                "enabled": true,
+                                "wavelength": 2
+                        },
+                        {
+                                "id": 1,
+                                "enabled": true,
+                                "wavelength": 2
+                        },
+                        {
+                                "id": 2,
+                                "enabled": true,
+                                "wavelength": 2
+                        },
+                        {
+                                "id": 3,
                                 "enabled": true,
                                 "wavelength": 2
                         }
@@ -94,13 +126,14 @@ function exec_put {
 # }
 
 function exec_delete {
-        curl -X DELETE --header ${header2} "http://${hostname}:${port}/api/arof"
+        curl -X DELETE --header ${header2} "http://${hostname}:${port}/arof"
 }
 
 echo "ENSURE LASERS OFF"
 exec_post
 echo "ENABLE LASERS"
-exec_put
+# exec_put
+exec_post_set
 echo "GET OPERATIONS ON LASERS"
 exec_get
 echo "DISABLE LASERS"
